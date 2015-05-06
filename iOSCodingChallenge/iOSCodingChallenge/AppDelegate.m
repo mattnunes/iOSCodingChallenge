4bc40e7712ebef390f9fe4e0e8f922cf1709a19a
Brian Voong <brian@touchofmodern.com>
2015-05-06 11:56:06 -0700

    Base project starting off point

---
+++ iOSCodingChallenge/iOSCodingChallenge/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  iOSCodingChallenge
+//
+//  Created by Administrator on 5/6/15.
+//  Copyright (c) 2015 Touch of Modern. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
---
+++ iOSCodingChallenge/iOSCodingChallenge/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSCodingChallenge
+//
+//  Created by Administrator on 5/6/15.
+//  Copyright (c) 2015 Touch of Modern. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+static NSString *const iosCodingChallengeUrlString = @"https://public.touchofmodern.com/ioschallenge.json";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //fetch json products and refresh view
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
